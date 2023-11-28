8f9ee906994fa77b6f9eba8100e74809f1622493
ahmed-yasser12 <ahmedyasser541@gmail.com>
2023-11-28 16:45:27 +0200

    footer done by me (ahmed yasser)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,60 @@
-export default function Footer(){
-    return <>
-        <h1>footer </h1>
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="bg-light">
+        <div className="container my-2  text-dark p-4">
+          <div className="row align-items-center ">
+            <div className="col-md-3">
+              <div className="content">
+                <div>
+                  <img src={imges} className="w-25 " alt="" />
+                </div>
+                <p className="my-2">Sakan@house.com</p>
+                <p className="my-2">+01202499897</p>
+                <p className="my-2">Lorem ipsum dolor sit amet consectetur.</p>
+                <div className="d-flex align-items-center justify-content-start ">
+                  <i className="fa-brands fa-facebook text-primary fs-3 mx-3"></i>
+                  <i class="fa-brands fa-instagram text-warning fs-3 mx-3"></i>
+                  <i class="fa-brands fa-twitter text-primary fs-3 mx-3"></i>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div>
+                <h4 className="fw-bolder mb-4">Expolre</h4>
+                <p className="text-muted">Buy a proprety</p>
+                <p className="text-muted">sell your proprety </p>
+                <p className="text-muted">Rent a proprety</p>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div>
+                <h4 className="fw-bolder mb-4">Resources</h4>
+                <p className="text-muted">vidoes</p>
+                <p className="text-muted">Member Stories</p>
+                <p className="text-muted">Our Homes&Phones</p>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div>
+                <h4 className="fw-bolder mb-4">About Us</h4>
+                <p className="text-muted">
+                  We provide a service to manage your property on one platform.
+                </p>
+                <p className="text-muted">
+                  Provides valuation .Sell .Buy .Rent <br /> iverst property
+                  ,and other real services
+                </p>
+                {/* <p className="text-muted"></p> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
+  );
 }
+
+export default Footer;
